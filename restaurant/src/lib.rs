1089cc87b6b4295d520192cf36e9155e1c9a5d71
zhanglun <zhanglun1410@gmail.com>
2020-08-29 13:27:52 +0800

    feat: restaurant

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,28 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn server_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // 绝对路径
+    // crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    // front_of_house::hosting::add_to_waitlist();
+
+    hosting::add_to_waitlist();
+    
+}
---
+++ restaurant/src/main.rs
@@ -0,0 +1,7 @@
+// use crate::front_of_house;
+use front_of_house::eat_at_restaurant;
+
+fn main() {
+  eat_at_restaurant();
+  // front_of_house::hosting::add_to_waitlist();
+}
