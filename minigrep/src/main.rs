f9d13b4bff27719cc520ad08d6e6aa9f0c6cbca7
zhanglun <zhanglun1410@gmail.com>
2020-08-30 18:56:26 +0800

    feat: mini grep step one

---
+++ minigrep/src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
+
+    println!("Searching for \"{}\"", config.query);
+    println!("In file {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+    
+    println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
+}
