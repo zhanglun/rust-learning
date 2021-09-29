61d20847e933b15552372d5fdede8b53b48556a2
zhanglun <zhanglun1410@gmail.com>
2021-09-29 14:32:59 +0000

    feat: trait demo

---
+++ trait-demo/src/main.rs
@@ -0,0 +1,57 @@
+trait AsJson {
+    fn as_json(&self) -> String;
+}
+
+fn send_data_as_json(value: &impl AsJson) {
+    println!("Sending JSON data to server...");
+    println!("-> {}", value.as_json());
+    println!("Done!\n");
+}
+
+struct Person {
+    name: String,
+    age: u8,
+    favorite_fruit: String,
+}
+
+struct Dog {
+    name: String,
+    color: String,
+    likes_petting: bool,
+}
+
+impl AsJson for Person {
+    fn as_json(&self) -> String {
+        format!(
+            r#"{{ "type": "person", "name": "{}", "age": {}, "favoriteFruit": "{}" }}"#,
+            self.name, self.age, self.favorite_fruit
+        )
+    }
+}
+
+impl AsJson for Dog {
+    fn as_json(&self) -> String {
+        format!(
+            r#"{{ "type": "dog", "name": "{}", "color": "{}", "likesPetting": {} }}"#,
+            self.name, self.color, self.likes_petting
+        )
+    }
+}
+
+fn main() {
+    println!("Hello, Trait!");
+    let laura = Person {
+        name: String::from("Laura"),
+        age: 31,
+        favorite_fruit: String::from("apples"),
+    };
+
+    let fido = Dog {
+        name: String::from("Fido"),
+        color: String::from("Black"),
+        likes_petting: true,
+    };
+
+    send_data_as_json(&laura);
+    send_data_as_json(&fido);
+}
