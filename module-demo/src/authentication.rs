29f82f0b081480105b37ddcf9bc0ef0e4e03f32b
zhanglun <zhanglun1410@gmail.com>
2021-09-30 12:11:02 +0000

    improvement: use third crates

---
+++ module-demo/src/authentication.rs
@@ -0,0 +1,30 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+pub struct User {
+    username: String,
+    password_hash: u64,
+}
+
+impl User {
+    pub fn new(username: &str, password: &str) -> User {
+        User {
+            username: username.to_string(),
+            password_hash: hash_password(&password.to_owned()),
+        }
+    }
+
+    pub fn get_username(&self) -> &String {
+        &self.username
+    }
+
+    pub fn set_password(&mut self, new_password: &str) {
+        self.password_hash = hash_password(&new_password.to_owned())
+    }
+}
+
+fn hash_password<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
--- module-demo/src/main.rs
+++ module-demo/src/main.rs
@@ -1,42 +1,48 @@
-mod authentication {
-    use std::collections::hash_map::DefaultHasher;
-    use std::hash::{Hash, Hasher};
+// mod authentication {
+//     use std::collections::hash_map::DefaultHasher;
+//     use std::hash::{Hash, Hasher};
 
-    pub struct User {
-        username: String,
-        password_hash: u64,
-    }
+//     pub struct User {
+//         username: String,
+//         password_hash: u64,
+//     }
 
-    impl User {
-        pub fn new(username: &str, password: &str) -> User {
-            User {
-                username: username.to_string(),
-                password_hash: hash_password(&password.to_owned()),
-            }
-        }
+//     impl User {
+//         pub fn new(username: &str, password: &str) -> User {
+//             User {
+//                 username: username.to_string(),
+//                 password_hash: hash_password(&password.to_owned()),
+//             }
+//         }
 
-        pub fn get_username(&self) -> &String {
-            &self.username
-        }
+//         pub fn get_username(&self) -> &String {
+//             &self.username
+//         }
 
-        pub fn set_password(&mut self, new_password: &str) {
-            self.password_hash = hash_password(&new_password.to_owned())
-        }
-    }
+//         pub fn set_password(&mut self, new_password: &str) {
+//             self.password_hash = hash_password(&new_password.to_owned())
+//         }
+//     }
 
-    fn hash_password<T: Hash>(t: &T) -> u64 {
-        let mut s = DefaultHasher::new();
-        t.hash(&mut s);
-        s.finish()
-    }
-}
+//     fn hash_password<T: Hash>(t: &T) -> u64 {
+//         let mut s = DefaultHasher::new();
+//         t.hash(&mut s);
+//         s.finish()
+//     }
+// }
+
+mod authentication;
+use regex::Regex;
 
 fn main() {
     println!("Hello, world!");
 
-    let user = authentication::User::new("jeremy", "super-secret");
+    let mut user = authentication::User::new("jeremy", "super-secret");
 
     println!("The username is: {}", user.get_username());
 
     user.set_password("even-more-secret");
+     
+    let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+    println!("Did our date match? {}", re.is_match("2014-01-01"));
 }
