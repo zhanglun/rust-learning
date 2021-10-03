428729d3384fcc7bc86e0ce204105b447dc18a03
zhanglun <zhanglun1410@gmail.com>
2021-10-03 13:25:04 +0800

    improvement: finish hash demo

--- hash-demo/src/main.rs
+++ hash-demo/src/main.rs
@@ -1,5 +1,5 @@
 #[derive(PartialEq, Debug)]
-struct Car { color: String, motor: Transmission, root: bool, age: (Age, u32) }
+struct Car { color: String, motor: Transmission, roof: bool, age: (Age, u32) }
 
 #[derive(PartialEq, Debug)]
 enum Transmission { Manual, SemiAuto, Automatic }
@@ -8,6 +8,73 @@ enum Transmission { Manual, SemiAuto, Automatic }
 enum Age { New, Used }
 
 
+fn car_quality (miles: u32) -> (Age, u32) {
+    if miles >0 {
+        return (Age::Used, miles);
+    }
+
+    (Age::New, miles)
+}
+
+
+fn car_factory(order: i32, miles: u32) -> Car {
+    let colors = ["Blue", "Green", "Red", "Silver"];
+
+    let mut color = order as usize;
+
+    if color > 4 {
+        color = color - 4;
+    }
+
+    let mut motor = Transmission::Manual;
+    let mut roof = true;
+    
+    if order % 3 == 0 {
+        motor = Transmission::Automatic;
+        roof = false;
+    } else if order % 2 == 0 {
+        motor = Transmission::SemiAuto;
+        roof = false;
+    }
+
+    Car {
+        color: String::from(colors[(color-1) as usize]),
+        motor: motor,
+        roof: roof,
+        age: car_quality(miles)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    let mut order = 1;
+    let mut car:Car;
+
+    car = car_factory(order, 1000);
+    println!("{}: {:?}, Hard top = {}, {:?}, {}, {} miles", order, car.age.0, car.roof, car.motor, car.color, car.age.1);
+
+    // Car order #2: Used, Convertible
+    order = order + 1;
+    car = car_factory(order, 2000);
+    println!("{}: {:?}, Hard top = {}, {:?}, {}, {} miles", order, car.age.0, car.roof, car.motor, car.color, car.age.1);    
+
+    // Car order #3: New, Hard top
+    order = order + 1;
+    car = car_factory(order, 0);
+    println!("{}: {:?}, Hard top = {}, {:?}, {}, {} miles", order, car.age.0, car.roof, car.motor, car.color, car.age.1);
+
+    // Car order #4: New, Convertible
+    order = order + 1;
+    car = car_factory(order, 0);
+    println!("{}: {:?}, Hard top = {}, {:?}, {}, {} miles", order, car.age.0, car.roof, car.motor, car.color, car.age.1);
+
+    // Car order #5: Used, Hard top
+    order = order + 1;
+    car = car_factory(order, 3000);
+    println!("{}: {:?}, Hard top = {}, {:?}, {}, {} miles", order, car.age.0, car.roof, car.motor, car.color, car.age.1);
+
+    // Car order #6: Used, Hard top
+    order = order + 1;
+    car = car_factory(order, 4000);
+    println!("{}: {:?}, Hard top = {}, {:?}, {}, {} miles", order, car.age.0, car.roof, car.motor, car.color, car.age.1);
 }
