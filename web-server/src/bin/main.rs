7efdf921304ecc60b55df72ba12c6947da4db815
zhanglun <zhanglun1410@gmail.com>
2021-10-10 19:09:31 +0800

    improvement: web server with multiple threads

--- web-server/src/bin/main.rs
+++ web-server/src/bin/main.rs
@@ -2,14 +2,19 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let _stream = stream.unwrap();
+        
 
-        handle_connection(_stream);
+        pool.execute(|| {
+            handle_connection(_stream);
+        });
     }
 }
 
