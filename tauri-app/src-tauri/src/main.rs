d4628fd7f2e5e3e793ae2d699476fbefe37e9c03
zhanglun <zhanglun1410@gmail.com>
2021-12-31 16:06:56 +0800

    improvement: test async

--- tauri-app/src-tauri/src/main.rs
+++ tauri-app/src-tauri/src/main.rs
@@ -8,13 +8,26 @@ use rss::Channel;
 use std::error::Error;
 
 #[tauri::command]
-fn my_custom_command() {
+fn my_custom_command1() {
   println!("I was invoked from JS!");
 }
 
 async fn request(url: &str) -> Result<String, Box<dyn Error>> {
   let content = reqwest::get(url).await?.text().await?;
   Ok(content.to_string())
+}
+
+#[tauri::command]
+async fn fetch_feed1(url: String) -> String {
+  let body = request(&url).await;
+  match body {
+    Ok(res) => {
+      return res.to_string();
+    }
+    Err(e) => {
+      panic!("==><>==>{:?}", e)
+    }
+  };
 }
 
 async fn request2(url: &str) -> Result<Channel, Box<dyn Error>> {
@@ -24,36 +37,29 @@ async fn request2(url: &str) -> Result<Channel, Box<dyn Error>> {
 }
 
 #[tauri::command]
-async fn fetch_feed1(url: String) -> String {
-  let body = request(&url).await;
-  let body = match body {
-    Ok(res) => {
-      return res.to_string();
-    },
-    Err(e) => {
-      panic!("====>{:?}", e)
-    }
+async fn fetch_feed2(url: String) -> Result<Channel, Box<dyn Error>> {
+  let result = request2(&url).await?;
+
+  if true {
+    return Ok(result);
+  } else {
+    return Err("asdf".into());
   };
 }
 
 #[tauri::command]
-async fn fetch_feed2(url: String) -> Channel {
-  let body = request2(&url).await;
-  let body = match body {
-    Ok(res) => {
-      println!("{:?}", res.title);
-      return res;
-    }
-    Err(e) => {
-      panic!("====>{:?}", e)
-    }
-  };
+async fn my_custom_command2(obj: String) {
+  println!("{:?}", obj);
 }
 
 fn main() {
-  print!("asdfasdf ===>");
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![fetch_feed1, fetch_feed2, my_custom_command])
+    .invoke_handler(tauri::generate_handler![
+      fetch_feed1,
+      fetch_feed2,
+      my_custom_command1,
+      my_custom_command2,
+    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- tauri-app/src/App.tsx
+++ tauri-app/src/App.tsx
@@ -1,50 +1,49 @@
-import React, { useEffect } from "react";
-import { invoke } from '@tauri-apps/api/tauri';
+import React, { useEffect, useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
 import logo from "./logo.svg";
 import tauriCircles from "./tauri.svg";
 import tauriWord from "./wordmark.svg";
 import "./App.css";
 
-const url = 'http://feed.appinn.com';
+const url = "http://feed.appinn.com";
 
 function App() {
-
+  const [res, setRes] = useState<any>("");
 
   const handleClick = async (n: number) => {
     const a = await invoke(`fetch_feed${n}`, { url });
-    console.log('a===<', a);
+    console.log("a===<", a);
+    setRes(a);
+  };
+
+  const handleClick2 = async (n: number) => {
+    const a = await invoke(`my_custom_command${n}`, {
+      number: 42,
+    });
+    console.log("handleClick2 ==>", a);
+  };
+
+  const request = () => {
+    fetch(url)
+      .then((res) => res.text())
+      .then((res) => {
+        setRes(res);
+      })
+      .catch((err) => {
+        setRes(err.message);
+      });
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="inline-logo">
-          <img src={tauriCircles} className="App-logo rotate" alt="logo" />
-          <img src={tauriWord} className="App-logo smaller" alt="logo" />
-        </div>
-        <a
-          className="App-link"
-          href="https://tauri.studio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Tauri
-        </a>
-        <img src={logo} className="App-logo rotate" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          <button onClick={() => handleClick(1)}>fetch feed</button>
-          <button onClick={() => handleClick(2)}>fetch feed 2</button>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+      <p>
+        <button onClick={() => handleClick(1)}>fetch feed</button>
+        <button onClick={() => handleClick(2)}>fetch feed 2</button>
+        <button onClick={() => handleClick2(1)}>my_custom_command 1</button>
+        <button onClick={() => handleClick2(2)}>my_custom_command 2</button>
+        <button onClick={() => request()}>request</button>
+      </p>
+      <p>{res}</p>
     </div>
   );
 }
