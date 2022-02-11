c0c0b7b8745784dcfcfa9b63b95763ed2accbf3e
zhanglun <zhanglun1410@gmail.com>
2022-02-11 06:02:01 +0000

    feat: add Menu

--- tauri-app/src-tauri/src/main.rs
+++ tauri-app/src-tauri/src/main.rs
@@ -6,7 +6,7 @@
 extern crate app;
 
 use self::app::*;
-use std::error::Error;
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
 #[tauri::command]
 async fn fetch_feed(url: String) -> String {
@@ -18,6 +18,41 @@ async fn fetch_feed(url: String) -> String {
 
   res
 }
+
+pub fn get_menu() -> Menu {
+  #[allow(unused_mut)]
+  let mut disable_item =
+    CustomMenuItem::new("disable-menu", "Disable menu").accelerator("CmdOrControl+D");
+  #[allow(unused_mut)]
+  let mut test_item = CustomMenuItem::new("test", "Test").accelerator("CmdOrControl+T");
+  #[cfg(target_os = "macos")]
+  {
+    disable_item = disable_item.native_image(tauri::NativeImage::MenuOnState);
+    test_item = test_item.native_image(tauri::NativeImage::Add);
+  }
+
+  let test_menu = Menu::new()
+    .add_item(CustomMenuItem::new(
+      "selected/disabled",
+      "Selected and disabled",
+    ))
+    .add_native_item(MenuItem::Separator)
+    .add_item(test_item);
+
+    let edit_menu = Menu::new()
+      .add_native_item(MenuItem::Copy)
+      .add_native_item(MenuItem::Paste)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::EnterFullScreen);
+
+  // add all our childs to the menu (order is how they'll appear)
+  Menu::new()
+    // .add_submenu(Submenu::new("My app", my_app_menu))
+    .add_submenu(Submenu::new("Other menu", test_menu))
+    .add_submenu(Submenu::new("Edit", edit_menu))
+}
+
+
 
 fn main() {
   tauri::Builder::default()
