1b7ba82c4108da00179d4e5095b26365b7d1f4d2
zhanglun <zhanglun1410@gmail.com>
2022-02-13 20:40:09 +0800

    improvement: update modal styles

--- tauri-app/src/components/AddFeedChannel/index.tsx
+++ tauri-app/src/components/AddFeedChannel/index.tsx
@@ -1,11 +1,12 @@
-import React, { useImperativeHandle, useState } from "react";
-import { http } from "@tauri-apps/api";
-import { Modal } from "../Modal";
-import { useModal } from "../Modal/useModal";
-import { db, Channel as ChannelModel, Article as ArticleModel } from "../../db";
+import React, {useImperativeHandle, useState} from "react";
+import {http} from "@tauri-apps/api";
+import {Modal} from "../Modal";
+import {useModal} from "../Modal/useModal";
+import {db, Channel as ChannelModel, Article as ArticleModel} from "../../db";
+import styles from './index.module.css';
 
 export const AddFeedChannel = (props: any) => {
-  const { showStatus, showModal, hideModal, toggleModal } = useModal();
+  const {showStatus, showModal, hideModal, toggleModal} = useModal();
   const [feedUrl, setFeedUrl] = useState("https://nodejs.org/en/feed/blog.xml");
   const [title, setTitle] = useState("");
   const [channel, setChannel] = useState({} as ChannelModel);
@@ -118,9 +119,9 @@ export const AddFeedChannel = (props: any) => {
         method: "GET",
         responseType: 2,
       })
-      .then(({ status, data }: any) => {
+      .then(({status, data}: any) => {
         if (status === 200) {
-          const { channel, items } = parseFeedXML(data);
+          const {channel, items} = parseFeedXML(data);
 
           setTitle(channel.title);
           setChannel(channel);
@@ -151,20 +152,23 @@ export const AddFeedChannel = (props: any) => {
   };
 
   return (
-    <Modal visible={showStatus} toggle={toggleModal} title="添加 RSS 订阅">
-      <div>
-        <div>
-          <div>Feed URL</div>
-          <input type="text" value={feedUrl} onChange={handleInputChange} />
-          <button onClick={handleLoad}>Load</button>
+    <Modal visible={showStatus} toggle={toggleModal} title="添加 RSS 订阅" onConfirm={handleSave} onCancel={handleCancel}>
+      <div className={styles.box}>
+        <div className={styles.item}>
+          <div className={styles.label}>Feed URL</div>
+          <div className={styles.formItem}>
+            <input type="text" value={feedUrl} onChange={handleInputChange}/>
+          </div>
+          <div className={styles.action}>
+            <button onClick={handleLoad}>Load</button>
+          </div>
         </div>
-        <div>
-          <div>title</div>
-          <input type="text" value={title} onChange={handleTitleChange} />
-        </div>
-        <div>
-          <button onClick={handleCancel}>cancel</button>
-          <button onClick={handleSave}>save</button>
+
+        <div className={styles.item}>
+          <div className={styles.label}>Title</div>
+          <div className={styles.formItem}>
+            <input type="text" value={title} onChange={handleTitleChange}/>
+          </div>
         </div>
       </div>
     </Modal>
