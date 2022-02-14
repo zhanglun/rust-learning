e8ec6957595f08c562e425d7647611569e70ee94
zhanglun <zhanglun1410@gmail.com>
2022-02-14 22:31:40 +0800

    chore: remove useless code

--- tauri-app/src/components/SettingPanel/index.tsx
+++ tauri-app/src/components/SettingPanel/index.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import { Tabs, TabPane } from '@douyinfe/semi-ui';
-// import { ChannelManagement } from './ChannelManagement';
-import { SettingSubscribe } from './Subscribe';
-import { ImportAndExport } from './ImportAndExport';
 import { MainHeader } from '../MainHeader';
 import styles from './settingpanel.module.css';
 
@@ -11,15 +7,6 @@ function SettingPanel() {
     <div className={styles.container}>
       <MainHeader title="设置" />
       <div className={styles.panelContainer}>
-        <Tabs type="line">
-          <TabPane tab="导入/导出" itemKey="2">
-            <SettingSubscribe />
-            <ImportAndExport />
-          </TabPane>
-          <TabPane tab="订阅管理" itemKey="1">
-            {/*<ChannelManagement />*/}
-          </TabPane>
-        </Tabs>
       </div>
     </div>
   );
