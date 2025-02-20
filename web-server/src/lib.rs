aa5211ea1ef898e93583d8a7cc2fdbc9c2b7a152
zhanglun <zhanglun1410@gmail.com>
2025-02-20 16:32:30 +0800

    feat: clean drop

--- web-server/src/lib.rs
+++ web-server/src/lib.rs
@@ -1,36 +1,43 @@
-use std::thread;
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::sync::Mutex;
+use std::thread;
 
 pub struct ThreadPool {
-  workers: Vec<Worker>,
-  sender: mpsc::Sender<Job>,
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 struct Worker {
-  id: usize,
-  thread: thread::JoinHandle<()>,
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-  fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-    let thread = thread::spawn(move || {
-      loop {
-        let job = receiver.lock().unwrap().recv().unwrap();
-        println!("Worker {} got a job; Executing.", id);
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv();
 
-        job();
-      }
-    });
+            match message {
+                Ok(job) => {
+                    println!("Worker {} got a job; Executing.", id);
 
-    Worker {
-      id,
-      thread,
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {} disconnected; shutting down.", id);
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
-  }
 }
 
 impl ThreadPool {
@@ -42,27 +49,42 @@ impl ThreadPool {
     ///
     /// `new` 函数在 size 为 0 时会 panic。
     pub fn new(size: usize) -> ThreadPool {
-      assert!(size > 0);
+        assert!(size > 0);
 
-      let (sender, receiver) = mpsc::channel();
-      let receiver = Arc::new(Mutex::new(receiver));
-      let mut workers = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
 
-      for id in 0..size {
-        workers.push(Worker::new(id, Arc::clone(&receiver)));
-      }
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
 
-      ThreadPool {
-        workers,
-        sender,
-      }
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
     }
 
-    pub fn execute<F>(&self, f:F)
-      where F: FnOnce() + Send + 'static
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
     {
-      let job = Box::new(f);
+        let job = Box::new(f);
 
-      self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
