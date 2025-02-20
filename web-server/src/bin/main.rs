bebe46680edc3fb2a66f4fed7e39623dd8d75edc
zhanglun <zhanglun1410@gmail.com>
2025-02-20 15:42:01 +0800

    fix: Option unwrap error

--- web-server/src/bin/main.rs
+++ web-server/src/bin/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::io::prelude::*;
+use std::io::BufReader;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use web_server::ThreadPool;
@@ -10,7 +11,6 @@ fn main() {
 
     for stream in listener.incoming() {
         let _stream = stream.unwrap();
-        
 
         pool.execute(|| {
             handle_connection(_stream);
@@ -19,25 +19,30 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    let get = b"GET / HTTP/1.1/\r\n";
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader
+        .lines()
+        .next()
+        .unwrap_or_else(|| Ok(String::from("")))
+        .unwrap();
 
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    println!("Request: {}", request_line);
+
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("index.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
     } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
-    };
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
 
-    let contents = fs::read_to_string(filename).unwrap();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n\n{contents}");
 
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        contents.len(),
-        contents
-    );
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
